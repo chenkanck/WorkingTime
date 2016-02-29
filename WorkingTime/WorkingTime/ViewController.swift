b1fb0b7337ff5e0cd5ed80144c1225441fc0a4f7
Kan Chen <kan@drchrono.com>
2016-02-29 15:09:47 -0800

    create project

---
+++ WorkingTime/WorkingTime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WorkingTime
+//
+//  Created by Kan Chen on 2/29/16.
+//  Copyright © 2016 Kan Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
